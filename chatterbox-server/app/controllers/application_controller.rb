25feaeb11edb1ef1924553f765ec656d72d7dff6
Jose Soto <jsoto7087@gmail.com>
2023-02-10 19:26:49 -0500

    complete

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,30 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+  use Rack::JSONBodyParser
+
+  get '/messages' do 
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end 
   
-  # Add routes
+  post '/messages' do 
+    message = Message.create(
+      username: params[:username],
+      body: params[:body]
+    )
+    message.to_json
+  end 
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id]).update(
+      body: params[:body]
+    )
+    message.to_json
+  end 
+
+  delete '/message/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+  end 
   
 end
---
+++ chatterbox-server/app/models/message.rb
@@ -0,0 +1,6 @@
+
+
+class Message < ActiveRecord::Base
+    has_many :messages
+  
+end
